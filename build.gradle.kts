400a3a50692b2e982bcd92cf88ede8d3331f210a
Zachary Wander <zachary.wander@gmail.com>
2020-11-29 22:02:33 -0500

    Update Compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 plugins {
     kotlin("jvm") version "1.4.20"
-    id("org.jetbrains.compose") version "0.2.0-build131"
+    id("org.jetbrains.compose") version "0.2.0-build132"
 }
 
 group = "tk.zwander"
